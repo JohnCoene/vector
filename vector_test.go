db27220a69f1c793c8a174e0a053ac3fbfbd9055
JohnCoene <jcoenep@gmail.com>
2020-11-08 19:09:33 +0100

    add test

--- vector_test.go
+++ vector_test.go
@@ -1,10 +1,12 @@
 package vector
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRandomVector(t *testing.T) {
-	RandomVector(1, 100)
+	p := RandomVector(1, 100)
+	fmt.Printf("Random vector x: %f, y: %f", p.X, p.Y)
 	RandomVectors(5, 1, 100)
 }
